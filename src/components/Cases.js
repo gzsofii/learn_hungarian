71e27fefe1f9b321ac02f0ff3f2926fad71d0555
kazozoka <gal.zsofi@gmail.com>
2021-02-16 10:49:05 +0100

    make hide and show all buttons work properly

--- src/components/Cases.js
+++ src/components/Cases.js
@@ -1,16 +1,23 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Case from './Case'
 import ModeContext, {MODE} from './../context/ModeContext'
 import cases from '../data/cases_data.js'
 
 
 export default function Cases() {
-    const [mode, setMode] = useState(MODE.hideAll) 
+    const [mode, setMode] = useState({visibility: MODE.hideAll, changed: false})
+     //mode is an object, because this way if we click on hide all, then click on some Values to make them show up, and then again click hide all,
+     // then in Value in useEffect it will detect as mode has changed (even though it's the same object).
+     // If it is only a string, the useEffect won't run as it doesn't detect change in mode
+
+   /* useEffect(() => {
+        setMode({changed: false})
+    }, [mode])*/
     return (
         <>
             <div>
-               <button onClick={() => setMode(MODE.showAll)}>Show all answers</button>
-               <button onClick={() => setMode(MODE.hideAll)}>Hide all answers</button>
+               <button onClick={() => setMode({visibility: MODE.showAll, changed: true})}>Show all answers</button>
+               <button onClick={() => setMode({visibility: MODE.hideAll, changed: true})}>Hide all answers</button>
             </div>
             <ModeContext.Provider value={mode}>
                 {console.log("Update ModeContext")}
--- src/components/Value.js
+++ src/components/Value.js
@@ -2,13 +2,13 @@ import React, {useContext, useEffect, useState} from 'react'
 import ModeContext, {MODE} from './../context/ModeContext'
 
 export default function Value(props) {
-    const mode = useContext(ModeContext)
-    const [hidden, setHidden] = useState(mode === MODE.showAll ? false : true)
+    const mode = useContext(ModeContext) // mode is an object: {visibility: ..., change: ...}
+    const [hidden, setHidden] = useState(mode.visibility === MODE.showAll ? false : true)
 
     useEffect(() => {
             console.log("mode changed to ", mode); 
-            if (mode === MODE.showAll) setHidden(false)
-            else if (mode === MODE.hideAll) setHidden(true)
+            if (mode.visibility === MODE.showAll) setHidden(false)
+            else if (mode.visibility === MODE.hideAll) setHidden(true)
         }, [mode])
 
     //console.log(hidden)
