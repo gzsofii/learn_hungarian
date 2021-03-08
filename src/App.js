528063fe86ce2b0361578af7c949c44bf0d6d883
gzsofii <gal.zsofi@gmail.com>
2021-03-08 11:32:26 +0100

    create base of menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.scss';
 import Cases from './components/Cases'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import menu from './/img/menu.png'
 
 function App() {
   return (
-    <div className="App">
-      <Cases/>
-    </div>
+    <Router>
+      <header>
+        <nav>
+          <img src={menu}/>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cases">Cases</Link>
+            </li>
+            <li>
+              <Link to="/verb_conjugation">Verb conjugation</Link>
+            </li>
+          </ul>
+        </nav>
+      
+        <Switch>
+          <Route path="/cases"><Cases/></Route>
+          <Route path="/verb_conjugation"><h1>Verb conjugation</h1></Route>
+          <Route path="/"><h1>Home</h1></Route>
+        </Switch>
+      </header>
+    </Router>
+      
   );
 }
 
