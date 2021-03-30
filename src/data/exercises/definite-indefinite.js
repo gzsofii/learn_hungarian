04ceddbb2be556a18819d68a2dbe62f17d9836b8
gzsofii <gal.zsofi@gmail.com>
2021-03-30 12:13:53 +0200

    add new exercise sentences

--- src/data/exercises/definite-indefinite.js
+++ src/data/exercises/definite-indefinite.js
@@ -11,33 +11,65 @@ Mennyi fát [láttok] (ti, látni)?
 const exercises = [
     {
         sentence: "_ az almát.",
-        words: "én, enni",
         solution: "eszem",
+        words: "én, enni",
         meaning: "I am eating the apple"
     },
     {
         sentence: "Almát _.",
-        words: "én, enni",
         solution: "eszek",
+        words: "én, enni",
         meaning: "I am eating apple"
     },
     {
         sentence: "_ rendőrt?",
-        words: "te, látni",
         solution: "látsz",
+        words: "te, látni",
         meaning: "Do you see any policemen?"
     },
     {
         sentence: "_ a rendőrt?",
-        words: "te, látni",
         solution: "látod",
+        words: "te, látni",
         meaning: "Do you see the policemen?"
     },
     {
-        sentence: "Mennyi fát _?",
-        words: "ti, látni",
-        solution: "láttok"
+        sentence: "A sárga házat _.",
+        solution: "keressük",
+        words: "mi, keresni",
+        meaning: "We are looking for the yellow house."
     },
+
+    {
+        sentence: "Egy sárga házat _.",
+        solution: "keresünk",
+        words: "mi, keresni",
+        meaning: "We are looking for a yellow house."
+    },
+    {
+        sentence: "_ egy új ruhát.",
+        solution: "vesz",
+        words: "ő, venni",
+        meaning: "She is buying a new dress."
+    },
+    {
+        sentence: "_ ezt a ruhát.",
+        solution: "megveszi",
+        words: "ő, megvenni",
+        meaning: "She is buying this dress."
+    },
+    {
+        sentence: "Mennyi fát _?",
+        solution: "láttok",
+        words: "ti, látni",
+        meaning: "How many trees do you see?"
+    },
+    /*{
+        sentence: "",
+        words: "",
+        solution: "",
+        meaning: ""
+    },*/
 
 ]
 
