34de98258947694beb6c1146443995f01afd3c69
gzsofii <gal.zsofi@gmail.com>
2021-04-09 12:11:12 +0200

    possessive exercise

---
+++ src/data/exercises/possessive.js
@@ -0,0 +1,75 @@
+const exercises = [
+    {
+        id: "1",
+        sentence: "Ez az autó_ az _.",
+        solution: [[""], ["enyém"]],
+        words: "én",
+        meaning: "This car is mine."
+    },
+    {
+        id: "2",
+        sentence: "Ez az _ autó_.",
+        solution: [["én", "", " "], ["m"]],
+        words: "én",
+        meaning: "This is my car."
+    },
+    {
+        id: "3",
+        sentence: "-Kié ez az autó? -Az _.",
+        solution: [["enyém", "én autóm"]],
+        words: "én",
+        meaning: "-Whose car is it? -Mine."
+    },
+    {
+        id: "4",
+        sentence: "Ez a ház_ Peti_",
+        solution: [["", " "], ["é"]],
+        words: "Peti",
+        meaning: "This house is Peti's"
+    },
+    {
+        id: "5",
+        sentence: "Ez Peti_ ház_.",
+        solution: [["", " "], ["a"]],
+        words: "Peti",
+        meaning: "This is Peti's house"
+    },
+    {
+        id: "6",
+        sentence: "-Kié ez a ház? -_",
+        solution: [["Petié", "a Petié"]],
+        words: "Peti",
+        meaning: "-Whose house is it? -Peti's"
+    },
+    {
+        id: "7",
+        sentence: " ",
+        solution: [[""]],
+        words: "",
+        meaning: ""
+    },
+    {
+        id: "8",
+        sentence: "",
+        solution: [[""]],
+        words: "",
+        meaning: ""
+    },
+    {
+        id: "9",
+        sentence: "",
+        solution: [[""]],
+        words: "",
+        meaning: ""
+    },
+    {
+        id: "10",
+        sentence: "",
+        solution: [[""]],
+        words: "",
+        meaning: ""
+    },
+
+]
+
+export default exercises;
